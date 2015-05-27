aa5baa9de9767cad12ab22731542df6e07f24ad1
Arastoa <waninokow@gmail.com>
2015-05-27 13:31:19 +0900

    Create common_header.h

---
+++ program_language/LL-1/common_header.h
@@ -0,0 +1,14 @@
+#ifndef COMMONHEADER
+#define COMMONHEADER
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#endif
